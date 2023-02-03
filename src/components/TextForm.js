33a655552df565b59146a11b4daa40680c8ed3f7
Brijesh Kumar Vishwakarma <117321656+BKV1996@users.noreply.github.com>
2023-02-03 17:09:17 +0530

    button responsive

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,28 +1,29 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import './TextForm.css';
 
 // console.log(useState('Enter text here'))
 export default function TextForm(props) {
-    const handleUpClick =()=>{
-      //  console.log("Uppercase was clicked" + text);
+    const handleUpClick = () => {
+        //  console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleLoClick =()=>{
-      //  console.log("Uppercase was clicked" + text);
+    const handleLoClick = () => {
+        //  console.log("Uppercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText);
     }
-    const handleClearClick =()=>{
-      //  console.log("Uppercase was clicked" + text);
+    const handleClearClick = () => {
+        //  console.log("Uppercase was clicked" + text);
         let newText = "";
         setText(newText);
     }
-    
-    const handleOnChange =(Event)=>{
-       // console.log("ON Change");
+
+    const handleOnChange = (Event) => {
+        // console.log("ON Change");
         setText(Event.target.value);
     }
-    const handleCopy =()=>{
+    const handleCopy = () => {
         //console.log("I am Copying")
         const text = document.getElementById("myBox");
         text.select();
@@ -39,24 +40,29 @@ export default function TextForm(props) {
     // setText ("new text"); // Correct way to change the state 
     return (
         <>
-        <div className='container'>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} id="myBox" onChange={handleOnChange} rows="8"></textarea>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} id="myBox" onChange={handleOnChange} rows="8"></textarea>
+                </div>
+
+                <div class="d-grid gap-2 d-md-block">
+                <button className="btn btn-primary mx-2" type='button' onClick={handleUpClick} >Convert to uppercase</button>
+                <button className="btn btn-primary mx-2" type='button' onClick={handleLoClick} >Convert to lowercase</button>
+                <button className="btn btn-primary mx-2" type='button' onClick={handleClearClick} >Clear Text</button>
+                <button className="btn btn-primary mx-2" type='button' onClick={handleCopy} >Copy Text</button>
+                <button className="btn btn-primary mx-2" type='button' onClick={handleExtraSpaces} >Remove Extra Spaces</button>
+                </div>
+
+
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick} >Convert to uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick} >Convert to lowercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick} >Clear Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleCopy} >Copy Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
-        </div>
-        <div className="container">
-            <h1>Your Text Summary</h1>
-            <p>{text.split(" ").length} words and {text.length} character</p>
-            <p> {0.008 * text.split(" ").length} Minute character read </p>
-            <h2>Preview</h2>
-            <p>{text}</p>
-        </div>
+            <div className="container">
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length} words and {text.length} character</p>
+                <p> {0.008 * text.split(" ").length} Minute character read </p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
