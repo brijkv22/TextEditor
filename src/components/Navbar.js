d63095f4402254d6cd2a8312167f0f3286e046cc
Brijesh Kumar Vishwakarma <117321656+BKV1996@users.noreply.github.com>
2023-03-28 16:58:27 +0530

    md5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <a href="/" className="navbar-brand" ><b>{props.title}</b></a>
+                <a href="/TextEditor" className="navbar-brand" ><b>{props.title}</b></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                            <a href="/" className="nav-link active" aria-current="page" ><b>Home</b></a>
+                            <a href="/TextEditor" className="nav-link active" aria-current="page" ><b>Home</b></a>
                         </li>
                         <li className="nav-item">
                             <a href="/textForm" className="nav-link active" aria-current="page" ><b>Text Editor</b></a>
